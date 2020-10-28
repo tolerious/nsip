3fa2dc35e60a5404dd03b5a69f78621d4463a4c1
helong.feng <tolerious@qq.com>
2020-10-28 12:52:41 +0800

    :construction: return 401 response successfully.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var sipRequest = require("./parseRequest.js");
 const dgram = require("dgram");
+const { request } = require("http");
 
 const server = dgram.createSocket("udp4");
 
@@ -7,20 +8,34 @@ server.on("message", (msg, rinfo) => {
   let siprequest = new sipRequest(msg);
   let str = `%c##############################Received##############################
 ${siprequest.sipString}
-####################################################################
-  `;
+####################################################################`;
   console.log(str, "color:gold");
-
-  console.log(siprequest.generate401Response());
-  server.send(
-    // Buffer.from(siprequest.generate401Response()),
-    Buffer.from(siprequest.generate401Response()),
-    5060,
-    "192.168.13.100",
-    (err) => {
-      console.log(err);
-    }
-  );
+  /**
+   * 判断是什么命令
+   */
+  let requestType = siprequest.getRequestType();
+  console.log(requestType);
+  switch (requestType) {
+    case "REGISTER":
+      if (siprequest.sipString.includes("Authorization") > 0) {
+        // 说明已经发送过401请求了， 需要发送200请求
+      } else {
+        console.log(".....");
+        // 说明没有发送过401请求，在这里进行发送
+        server.send(
+          // Buffer.from(siprequest.generate401Response()),
+          siprequest.generate401Response(),
+          5060,
+          "192.168.13.100",
+          (err) => {
+            console.log(`error:${err}`);
+          }
+        );
+      }
+      break;
+    default:
+      break;
+  }
 });
 
 server.on("listening", () => {
--- src/parseRequest.js
+++ src/parseRequest.js
@@ -4,61 +4,77 @@ class sipRequest {
     this.sipString = sipString.toString();
     this.sipStringParse2Array = this.sipString.split("\r\n");
     this.firstLine = this.sipStringParse2Array[0];
+    this.username = "";
+    this.clientServer = "";
   }
   /**
+   * 获取请求类型
    * answer 代表是“应答”
    * request 代表是“请求”
    */
   getRequestType() {
     if (this.firstLine.split(" ").includes("SIP") > 0) {
+      // 说明是响应
       return "answer";
     } else {
-      return "request";
+      //  说明是命令
+      let type = this.firstLine.split(" ")[0];
+      return type;
     }
   }
-  getFromTag() {
-    let fromString = this.sipStringParse2Array[2].split(";")[1].split("=")[1];
-    return fromString;
+  /**
+   * 获取向服务器发起注册的客户端的地址信息,注意只在客户端往服务器发送的消息中有效
+   */
+  getSipClientSipAddress() {
+    let middle = this.firstLine.split(" ")[1];
+    return middle;
   }
-  getMethod() {
-    let method = this.firstLine.split(" ")[0];
-    return method;
+  getSipClientAddress() {
+    let m = this.getSipClientSipAddress().replace("spi:", "");
+    [this.username, this.clientServer] = m.split("@");
   }
-  getSipVersion() {
-    let version = this.firstLine.split(" ")[2];
-    return version;
+  getFromeString() {
+    return this.sipStringParse2Array[2];
+  }
+  getToString() {
+    return this.sipStringParse2Array[3];
+  }
+  getCallID() {
+    return this.sipStringParse2Array[4];
+  }
+  getViaBranch() {
+    return this.sipStringParse2Array[1].split("=")[1];
   }
   generate401Response() {
+    this.getSipClientAddress();
     let firstLine = "SIP/2.0 401 Unauthorized" + "\r\n";
-    let toString =
-      "To: <sip:340200000013200000001@192.168.13.100:5060>" +
-      "\r\n";
-    let fromString =
-      "From: <sip:340200000013200000001@192.168.13.100:5060>;tag=" +
-      this.getFromTag() +
-      "\r\n";
-    let Cseq = "Cseq: 1 REGISTER" + "\r\n";
+    let toString = "<sip:" + this.getSipClientSipAddress() + ">" + "\r\n";
+    let fromString = "<sip:" + this.getSipClientSipAddress() + ">" + "\r\n";
+    let Cseq = "CSeq: 1 REGISTER" + "\r\n";
     let VIA =
-      "Via: SIP/2.0/UDP 192.168.13.100:5060;rport=5060;branch=z9hG4bK" +
-      uuid.v4() +
+      "Via: SIP/2.0/UDP 192.168.13.100:5060;rport=5060;branch=" +
+      this.getViaBranch() +
+      ";received=192.168.13.100" +
       "\r\n";
-    let callID = "Call-ID: " + uuid.v4() + "@192.168.13.100" + "\r\n";
-    let MaxForwards = "Max-Forwards: 70" + "\r\n";
+    let callID = this.getCallID() + "\r\n";
     let wwwAuthenticate =
-      `WWW-Authenticate: Digest username="34020000002000000001", realm="3402000000", nonce="bd2e4df9e3d9b280", uri="sip:64000000002000000001@192.168.13.100:5060", response="9c8411f2b96c5aef55eb136ba3f34655", algorithm=MD5` +
+      'WWW-Authenticate: Digest realm="64010000",nonce="6fe9ba44a76be22a",algorithm=MD5' +
       "\r\n";
-    let contentLength = "Content-Length: 0" + "\r\n" + "\r\n";
-    return (
+    let contentLength = "Content-Length: 0" + "\r\n";
+    let finalStr =
       firstLine +
       VIA +
-      toString +
       fromString +
+      toString +
       Cseq +
       callID +
-      MaxForwards +
       wwwAuthenticate +
-      contentLength
-    );
+      contentLength +
+      "\r\n";
+    console.log(`#########generate 401 response#########`);
+    console.log(finalStr);
+    console.log(`#########end 401 response#########`);
+    return finalStr;
   }
   /**
    * TODO: 实现可以处理多个头域行的情况和用逗号分割的合并的单个头域行的情况。 RFC3261 7.3.1
